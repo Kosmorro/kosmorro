62f7304a68addaec615ee3e694469ef521bdbd7c
Jérôme Deuchnord <jerome@deuchnord.fr>
2021-06-20 17:23:34 +0200

    feat: replace latitude and longitude with one argument alone: --position

--- .scripts/tests-e2e.sh
+++ .scripts/tests-e2e.sh
@@ -91,28 +91,34 @@ assertSuccess "$KOSMORRO_COMMAND --date='+3y 5m3d'"
 assertSuccess "$KOSMORRO_COMMAND --date='-1y3d'"
 assertFailure "$KOSMORRO_COMMAND --date='+3d4m"
 assertFailure "$KOSMORRO_COMMAND -date='3y'"
-assertSuccess "$KOSMORRO_COMMAND --latitude=50.5876 --longitude=3.0624"
-assertSuccess "$KOSMORRO_COMMAND --latitude=50.5876 --longitude=3.0624 -d 2020-01-27"
-assertSuccess "$KOSMORRO_COMMAND --latitude=50.5876 --longitude=3.0624 -d 2020-01-27 --timezone=1"
-assertSuccess "$KOSMORRO_COMMAND --latitude=50.5876 --longitude=3.0624 -d 2020-01-27 --timezone=-1"
-assertSuccess "$KOSMORRO_COMMAND --latitude=50.5876 --longitude=3.0624 -d 2020-01-27 --format=json"
-assertFailure "$KOSMORRO_COMMAND --latitude=50.5876 --longitude=3.0624 -d 2020-01-27 --format=pdf"
+assertFailure "$KOSMORRO_COMMAND --latitude=50.5876 --longitude=3.0624"
+assertSuccess "$KOSMORRO_COMMAND --position=\"50.5876;3.0624\""
+assertSuccess "$KOSMORRO_COMMAND --position=\"50.5876,3.0624\""
+assertSuccess "$KOSMORRO_COMMAND --position=\"50.5876;-3.0624\""
+assertSuccess "$KOSMORRO_COMMAND --position=\"-50.5876;-3.0624\""
+assertSuccess "$KOSMORRO_COMMAND --position=\"50.5876,-3.0624\""
+assertSuccess "$KOSMORRO_COMMAND --position=\"-50.5876,-3.0624\""
+assertSuccess "$KOSMORRO_COMMAND --position=\"50.5876;3.0624\" -d 2020-01-27"
+assertSuccess "$KOSMORRO_COMMAND --position=\"50.5876;3.0624\" -d 2020-01-27 --timezone=1"
+assertSuccess "$KOSMORRO_COMMAND --position=\"50.5876;3.0624\" -d 2020-01-27 --timezone=-1"
+assertSuccess "$KOSMORRO_COMMAND --position=\"50.5876;3.0624\" -d 2020-01-27 --format=json"
+assertFailure "$KOSMORRO_COMMAND --position=\"50.5876;3.0624\" -d 2020-01-27 --format=pdf"
 
 # Environment variables
 assertSuccess "LATITUDE=50.5876 LONGITUDE=3.0624 TIMEZONE=1 kosmorro -d 2020-01-27"
 assertSuccess "LATITUDE=50.5876 LONGITUDE=3.0624 TIMEZONE=-1 kosmorro -d 2020-01-27"
 
 # Missing dependencies, should fail
-assertFailure "$KOSMORRO_COMMAND --latitude=50.5876 --longitude=3.0624 -d 2020-01-27 --format=pdf -o $HOME/kosmorro/export/document.pdf"
+assertFailure "$KOSMORRO_COMMAND --position=\"50.5876;3.0624\" -d 2020-01-27 --format=pdf -o $HOME/kosmorro/export/document.pdf"
 assertFailure "ls $HOME/kosmorro/export/document.pdf"
 
 assertSuccess "sudo apt-get install -y texlive texlive-latex-extra" "CI"
 
 # Dependencies installed, should not fail
-assertSuccess "$KOSMORRO_COMMAND --latitude=50.5876 --longitude=3.0624 -d 2020-01-27 --format=pdf -o $HOME/kosmorro/export/document.pdf"
+assertSuccess "$KOSMORRO_COMMAND --position=\"50.5876;3.0624\" -d 2020-01-27 --format=pdf -o $HOME/kosmorro/export/document.pdf"
 assertSuccess "ls $HOME/kosmorro/export/document.pdf"
 
-assertSuccess "$KOSMORRO_COMMAND --latitude=50.5876 --longitude=3.0624 -d 2020-01-27 --format=pdf -o $HOME/kosmorro/export/document-no-graph.pdf --no-graph"
+assertSuccess "$KOSMORRO_COMMAND --position=\"50.5876;3.0624\" -d 2020-01-27 --format=pdf -o $HOME/kosmorro/export/document-no-graph.pdf --no-graph"
 assertSuccess "ls $HOME/kosmorro/export/document-no-graph.pdf"
 
 # man page
---
+++ _kosmorro/geolocation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import re
+
+from kosmorrolib import Position
+
+from .i18n.utils import _
+
+
+def _parse_latitude_longitude(from_str: str) -> Position:
+    if not re.search(r"^([\d.-]+)[,;]([\d.-]+)$", from_str):
+        raise ValueError(_("The given position (%s) is not valid." % from_str))
+
+    latitude_longitude = from_str.split(';')
+    if len(latitude_longitude) == 1:
+        latitude_longitude = from_str.split(',')
+
+    return Position(float(latitude_longitude[0]), float(latitude_longitude[1]))
+
+
+def get_position(from_str: str) -> Position:
+    return _parse_latitude_longitude(from_str)
