d85ee27c2f5292a74bdc6596099f2cdd556ca09c
Jérôme Deuchnord <Deuchnord@users.noreply.github.com>
2021-08-11 19:07:04 +0200

    docs(license): revert to license GNU AGPL (#196)
    This reverts commit 6ffe0ad07b13a04ef4a5de959135d227c2b05ed3.

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
+#    Kosmorro - Compute The Next Ephemerides
+#    Copyright (C) 2019  Jérôme Deuchnord <jerome@deuchnord.fr>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import pathlib
 from setuptools import setup, find_packages
 
@@ -14,8 +30,8 @@ setup(
     author="Jérôme Deuchnord",
     author_email="jerome@deuchnord.fr",
     url="http://kosmorro.space",
-    license="CeCILL-v2.1",
-    description="A program to compute the ephemerides.",
+    license="AGPL-3.0",
+    description="A program that computes the ephemerides.",
     long_description=README,
     long_description_content_type="text/markdown",
     keywords="kosmorro astronomy ephemerides ephemeris",
