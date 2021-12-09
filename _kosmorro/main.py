6c5dc6fac63ad23e689aa8d9819128f0c7232193
Jérôme Deuchnord <jerome@deuchnord.fr>
2021-12-09 19:18:48 +0100

    feat: add support for `KOSMORRO_POSITION` environment variable

--- _kosmorro/main.py
+++ _kosmorro/main.py
@@ -56,7 +56,11 @@ def main():
         print(colored(error.args[0], color="red", attrs=["bold"]))
         return -1
 
-    position = get_position(args.position) if args.position not in [None, ""] else None
+    position = None
+    if args.position not in [None, ""]:
+        position = get_position(args.position)
+    elif env_vars.position not in [None, ""]:
+        position = get_position(env_vars.position)
 
     # if output format is not specified, try to use output file extension as output format
     if args.output is not None and output_format is None:
