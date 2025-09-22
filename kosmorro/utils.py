8a641961f7f6b3405c3658941c40e1a8a0c30975
Deuchnord <git@deuchnord.fr>
2025-09-22 15:40:02 +0200

    chore: update dependencies

--- kosmorro/utils.py
+++ kosmorro/utils.py
@@ -3,10 +3,7 @@
 from termcolor import colored as do_color
 from sys import stderr
 
-try:
-    from importlib.metadata import version
-except ImportError:
-    from importlib_metadata import version
+from importlib.metadata import version
 
 KOSMORRO_VERSION = version("kosmorro")
 KOSMORROLIB_VERSION = version("kosmorrolib")
